6b54ac23a1a934f8263cd994ec4bcb9ec7f7805c
LeonardoPSouzaPortfolio <leonardo.p.desouzadeveloper@gmail.com>
2017-11-21 20:19:56 -0200

    5.2. Desafio: Lista e busca de lançamentos

---
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/algamoney/api/repository/LancamentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.LeonardoPSouzaPortfolio.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.LeonardoPSouzaPortfolio.algamoney.api.model.Lancamento;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+
+}
+	
